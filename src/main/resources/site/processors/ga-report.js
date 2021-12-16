c9d04c8254fd857e5397cb68130530961c1cfa5b
Persijn Kwekkeboom <pkw@enonic.com>
2021-12-16 16:53:53 +0100

    Response processor null content fix

--- src/main/resources/site/processors/ga-report.js
+++ src/main/resources/site/processors/ga-report.js
@@ -1,46 +1,42 @@
-var contentLib = require('/lib/xp/content');
-var portalLib = require('/lib/xp/portal');
+// const contentLib = require('/lib/xp/content');
+const portalLib = require('/lib/xp/portal');
 
 exports.responseProcessor = function (req, res) {
-    var contentId = req.params.contentId;
-    if (!contentId) {
-        contentId = portalLib.getContent()._id;
-    }
-    var siteConfig = contentLib.getSiteConfig({
-        key: contentId,
-        applicationKey: app.name
-    });
-    var trackingID = siteConfig['trackingId'] || '';
-    var enableTracking = siteConfig['enableTracking'] || false;
-    var enableAnonymization = siteConfig['enableAnonymization'] || false;
+    const siteConfig = portalLib.getSiteConfig();
 
-    if (!trackingID || !enableTracking) {
-        return res;
-    }
+    if (siteConfig) {
+        var trackingID = siteConfig['trackingId'] || '';
+        var enableTracking = siteConfig['enableTracking'] || false;
+        var enableAnonymization = siteConfig['enableAnonymization'] || false;
 
-    if (req.mode !== 'live') {
-        return res;
-    }
+        if (!trackingID || !enableTracking) {
+            return res;
+        }
 
-    var snippet = '<!-- Google Analytics -->';
-    snippet += '<script>';
-    snippet += '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){';
-    snippet += '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),';
-    snippet += 'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)';
-    snippet += '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');';
-    snippet += 'ga(\'create\', \'' + trackingID + '\', \'auto\');';
-    snippet += 'ga(\'send\', \'pageview\'' + (enableAnonymization ? ', {\'anonymizeIp\': true}' : '') + ');';
-    snippet += '</script>';
-    snippet += '<!-- End Google Analytics -->';
+        if (req.mode !== 'live') {
+            return res;
+        }
 
-    var headEnd = res.pageContributions.headEnd;
-    if (!headEnd) {
-        res.pageContributions.headEnd = [];
-    }
-    else if (typeof(headEnd) == 'string') {
-        res.pageContributions.headEnd = [headEnd];
-    }
+        var snippet = '<!-- Google Analytics -->';
+        snippet += '<script>';
+        snippet += '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){';
+        snippet += '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),';
+        snippet += 'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)';
+        snippet += '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');';
+        snippet += 'ga(\'create\', \'' + trackingID + '\', \'auto\');';
+        snippet += 'ga(\'send\', \'pageview\'' + (enableAnonymization ? ', {\'anonymizeIp\': true}' : '') + ');';
+        snippet += '</script>';
+        snippet += '<!-- End Google Analytics -->';
 
-    res.pageContributions.headEnd.push(snippet);
+        var headEnd = res.pageContributions.headEnd;
+        if (!headEnd) {
+            res.pageContributions.headEnd = [];
+        }
+        else if (typeof(headEnd) == 'string') {
+            res.pageContributions.headEnd = [headEnd];
+        }
+
+        res.pageContributions.headEnd.push(snippet);
+    }
     return res;
 };
